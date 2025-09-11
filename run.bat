b551e42704be616dbb403ec6faf8379ea25acc55
Nasir Mustapha <teboy.com@gmail.com>
2025-09-11 07:36:01 +0100

    refactor: comment out fullscreen kiosk mode launch for Edge; ensure app mode is used

--- run.bat
+++ run.bat
@@ -4,4 +4,5 @@ start /MIN "" flask run
 timeout /t 5 /nobreak
 
 :: Launch Edge in fullscreen kiosk mode
-start "" "msedge" --kiosk --edge-kiosk-type=fullscreen --no-first-run --disable-features=msEdgeEnterpriseModePolicies http://127.0.0.1:5000
+:: start "" "msedge" --kiosk --edge-kiosk-type=fullscreen --no-first-run --disable-features=msEdgeEnterpriseModePolicies http://127.0.0.1:5000
+start "" "msedge" --app=http://127.0.0.1:5000
